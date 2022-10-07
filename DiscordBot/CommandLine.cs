1427b815bd631b08b8782ec89310794b74d87390
DESKTOP-7S6MQS2\Admin <hanq@robotcom-fa.com>
2022-10-07 17:46:04 +0700

    Main

---
+++ DiscordBot/CommandLine.cs
@@ -0,0 +1,20 @@
+﻿using Discord;
+using Discord.Commands;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace myDiscordBot.Modules
+{
+    public class CommandLine : ModuleBase
+    {
+        [Command("Hello")]
+        public async Task Hello()
+        {
+            await ReplyAsync("Hi! Chào mừng " + Context.Message.Author.Mention + " đến với GkzCoding. ");
+        }
+    }
+}
